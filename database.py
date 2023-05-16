e31c2f39bbb0ddb4fd43416cbaa7157826a0dbe0
Danial <bidaybek044@gmail.com>
2023-05-16 13:44:26 +0600

    updated

--- database.py
+++ database.py
@@ -1,7 +1,29 @@
 import os
+from typing import Protocol
 
 import psycopg2
 from dotenv import load_dotenv
+
+load_dotenv()
+host = os.getenv('HOST')
+database = os.getenv('DATABASE')
+user = os.getenv('USER')
+password = os.getenv('PASSWORD')
+
+# print(host, database, user, password)
+
+db_config = {
+    'user': user,
+    'host': host,
+    'port': 5433,
+    'database': database,
+    'password': password
+}
+
+
+class Database(Protocol):
+
+    def save_pdf_data(self, pdf_name: str, data: str): ...
 
 
 class DatabasePostgres:
@@ -9,14 +31,7 @@ class DatabasePostgres:
         self.db_config = db_config
 
     def save_pdf_data(self, pdf_name: str, data: str):
-        print(self.db_config)
-        conn = psycopg2.connect(
-            host=self.db_config.get('host'),
-            port=self.db_config.get('port'),
-            database=self.db_config.get('database'),
-            user=self.db_config.get('user'),
-            password=self.db_config.get('password')
-        )
+        conn = self._connect()
         cursor = conn.cursor()
 
         cursor.execute(f'INSERT INTO public.pdf_db (pdf_name, pdf_body) VALUES (%s, %s)', (pdf_name, data))
@@ -25,22 +40,18 @@ class DatabasePostgres:
         cursor.close()
         conn.close()
 
+    def _connect(self):
+        conn = psycopg2.connect(
+            host=self.db_config.get('host'),
+            port=self.db_config.get('port'),
+            database=self.db_config.get('database'),
+            user=self.db_config.get('user'),
+            password=self.db_config.get('password')
+        )
 
-def db_setup() -> DatabasePostgres:
-    load_dotenv()
-    host = os.getenv('HOST')
-    database = os.getenv('DATABASE')
-    user = os.getenv('USER')
-    password = os.getenv('PASSWORD')
+        return conn
 
-    # print(host, database, user, password)
 
-    db_config = {
-        'user': user,
-        'host': host,
-        'port': 5433,
-        'database': database,
-        'password': password
-    }
+def db_setup() -> Database:
 
     return DatabasePostgres(db_config=db_config)
